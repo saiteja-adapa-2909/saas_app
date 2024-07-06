203d242e7a2e8b4c8c4f6727423be5ff069a700a
saiteja-adapa-2909 <98104890+saiteja-adapa-2909@users.noreply.github.com>
2024-07-06 13:56:37 +0530

    Update links.js

--- saas-app/src/links.js
+++ saas-app/src/links.js
@@ -1,3 +1,4 @@
-const baseURL = "https://saas-app-res3.onrender.com";
+// const baseURL = "https://saas-app-res3.onrender.com";
+const baseURL = "https://saas-app-1.onrender.com";
 
 export default baseURL;
